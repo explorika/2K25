c17df370563372439ea2fa3ab66500d2e0a4841c
explorika <explorika2k25@gmail.com>
2025-04-25 09:07:59 +0530

    Update script.js

--- css/script.js
+++ css/script.js
@@ -6,28 +6,8 @@ navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Countdown Timer
-const countdownDate = new Date('April 22, 2025 8:00:00').getTime();
 
-function updateCountdown() {
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById('days').textContent = String(days).padStart(2, '0');
-    document.getElementById('hours').textContent = String(hours).padStart(2, '0');
-    document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
-    document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
-
-    if (distance < 0) {
-        clearInterval(countdownInterval);
-         document.getElementById('countdown').innerHTML = '<h2>The Event Has Started! <br> Day 1: Ethnic Day</h2>';
-    }
-}
 
 const countdownInterval = setInterval(updateCountdown, 1000);
 updateCountdown();
